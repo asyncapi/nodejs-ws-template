e379adc39ea5db0b5fd1228f87355c7ea1cc2886
Olivier Lechevalier <RageZBla@users.noreply.github.com>
2020-10-01 21:14:48 +0900

    Remove unnecessary semicolon from filters

--- filters/all.js
+++ filters/all.js
@@ -6,10 +6,10 @@ const path = require('path');
 function port(url, defaultPort) {
   const parsed = URL.parse(url);
   return parsed.port || defaultPort || 80;
-};
+}
 filter.port = port;
 
 function pathResolve(pathName, basePath = '/') {
   return path.resolve(basePath, pathName);
-};
+}
 filter.pathResolve = pathResolve;
