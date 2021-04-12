6baf92b20a7a176e4ffc36aada6e2bc1f0c9b572
Jonas Lagoni <jonas-lt@live.dk>
2021-04-12 15:21:40 +0200

    Switched to new intent API

--- template/src/api/services/$$channel$$.js
+++ template/src/api/services/$$channel$$.js
@@ -1,17 +1,19 @@
 const service = module.exports = {};
-{% if channel.hasPublish() %}
+
+{%- for operation in channel.operations() %}
+{% if operation.isClientPublishing() %}
 /**
- * {{ channel.publish().summary() }}
+ * {{ operation.summary() }}
  * @param {object} ws WebSocket connection.
  * @param {object} options
- * @param {%raw%}{{%endraw%}{{channel.publish().message(0).payload().type()}}{%raw%}}{%endraw%} options.message The message to send.
-{%- if channel.publish().message(0).headers() %}
-{%- for fieldName, field in channel.publish().message(0).headers().properties() %}
+ * @param {%raw%}{{%endraw%}{{operation.messages()[0].payload().type()}}{%raw%}}{%endraw%} options.message The message to send.
+{%- if operation.messages()[0].headers() %}
+{%- for fieldName, field in operation.messages()[0].headers().properties() %}
 {{ field | docline(fieldName, 'options.message.headers') }}
 {%- endfor %}
 {%- endif %}
-{%- if channel.publish().message(0).payload() %}
-{%- for fieldName, field in channel.publish().message(0).payload().properties() %}
+{%- if operation.messages()[0].payload() %}
+{%- for fieldName, field in operation.messages()[0].payload().properties() %}
 {{ field | docline(fieldName, 'options.message.payload') }}
 {%- endfor %}
 {%- endif %}
@@ -19,29 +21,29 @@ const service = module.exports = {};
 service.{{ channel.publish().id() }} = async (ws, { message }) => {
   ws.send('Message from the server: Implement your business logic here.');
 };
-
 {%- endif %}
-{%- if channel.hasSubscribe() %}
+
+{%- if operation.isClientSubscribing() %}
 /**
- * {{ channel.subscribe().summary() }}
+ * {{ operation.summary() }}
  * @param {object} ws WebSocket connection.
  * @param {object} options
  * @param {string} options.path The path in which the message was received.
  * @param {object} options.query The query parameters used when connecting to the server.
- * @param {%raw%}{{%endraw%}{{channel.subscribe().message(0).payload().type()}}{%raw%}}{%endraw%} options.message The received message.
-{%- if channel.subscribe().message(0).headers() %}
-{%- for fieldName, field in channel.subscribe().message(0).headers().properties() %}
+ * @param {%raw%}{{%endraw%}{{operation.messages()[0].payload().type()}}{%raw%}}{%endraw%} options.message The received message.
+{%- if operation.messages()[0].headers() %}
+{%- for fieldName, field in operation.messages()[0].headers().properties() %}
 {{ field | docline(fieldName, 'options.message.headers') }}
 {%- endfor %}
 {%- endif %}
-{%- if channel.subscribe().message(0).payload() %}
-{%- for fieldName, field in channel.subscribe().message(0).payload().properties() %}
+{%- if operation.messages()[0].payload() %}
+{%- for fieldName, field in operation.messages()[0].payload().properties() %}
 {{ field | docline(fieldName, 'options.message.payload') }}
 {%- endfor %}
 {%- endif %}
  */
-service.{{ channel.subscribe().id() }} = async (ws, { message, path }) => {
+service.{{ operation.id() }} = async (ws, { message, path }) => {
   ws.send('Message from the server: Implement your business logic here.');
 };
-
 {%- endif %}
+{% endfor %}
