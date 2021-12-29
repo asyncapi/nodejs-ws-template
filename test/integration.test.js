3058b24b5bfd8e98c96fc561fde39aa1b2522e11
Krishna Kumar S <71367204+Krishks369@users.noreply.github.com>
2021-12-29 21:06:25 +0530

    Created jest test

---
+++ test/integration.test.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const Generator = require('@asyncapi/generator');
+const {readFile} = require('fs').promises;
+const crypto = require('crypto');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const MAIN_TEST_RESULT_PATH = path.join('test', 'temp',' integrationTestResult');
+const URL = 'https://raw.githubusercontent.com/Krishks369/shrekapp-asyncapi-designed/main/asyncapi.yaml'
+
+describe('template integration test using generator', () => {
+    const generateFolderName = () =>{
+        return path.resolve(MAIN_TEST_RESULT_PATH, Date.now().toString());
+    };
+
+    jest.setTimeout(30000);
+
+it('should generate application files ', async () =>{
+    const outputDir = generateFolderName();
+    const asyncapiFile = await fetch(URL);
+
+    const params ={
+        server: 'swamp'
+    }
+
+
+
+const generator = new Generator(path.normalize('./'), outputDir, {forceWrite: true, templateParams: params} );
+await generator.generateFromString( await asyncapiFile.text()) ;    
+const expectedFiles = [
+    'README.md',
+    'template/src/api/index.js',
+    'template/src/api/routes.js',
+    'template/src/lib/asyncapi.js',
+    'template/src/lib/colors.js',
+    'template/src/lib/config.js',
+    'template/src/lib/path.js'
+
+
+
+];
+
+for (const index in expectedFiles){
+    const file = await readFile( expectedFiles[index], 'utf8');
+    expect(file).toMatchSnapshot();
+}
+
+
+});
+});
