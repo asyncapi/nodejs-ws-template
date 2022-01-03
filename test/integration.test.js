92fee5b0f9e25af3c394e3c51dc5afa7393b5c2b
Krishna Kumar S <71367204+Krishks369@users.noreply.github.com>
2022-01-03 19:52:27 +0530

    Updated changes

--- test/integration.test.js
+++ test/integration.test.js
@@ -3,6 +3,7 @@ const Generator = require('@asyncapi/generator');
 const {readFile} = require('fs').promises;
 const fetch = require('node-fetch');
 const fs = require('fs');
+const console = require('console');
 
 const MAIN_TEST_RESULT_PATH = path.join('test', 'temp', ' integrationTestResult');
 const URL = 'https://raw.githubusercontent.com/asyncapi/generator/master/test/docs/ws.yml'
@@ -24,19 +25,19 @@ describe('template integration test using generator', () => {
 			forceWrite: true,
 			templateParams: params
 		});
+		console.log(outputDir);
 		await generator.generateFromString(await asyncapiFile.text());
 		const expectedFiles = [
-			'README.md',
-			'template/src/api/index.js',
-			'template/src/api/routes.js',
-			'template/src/lib/asyncapi.js',
-			'template/src/lib/colors.js',
-			'template/src/lib/config.js',
-			'template/src/lib/path.js'
+			'src/api/index.js',
+			'src/api/routes.js',
+			'src/lib/asyncapi.js',
+			'src/lib/colors.js',
+			'src/lib/config.js',
+			'src/lib/path.js'
         ];
 
 		for (const index in expectedFiles) {
-			const file = await readFile(expectedFiles[index], 'utf8');
+			const file = await readFile(path.join(outputDir, expectedFiles[index]), 'utf8');
 			expect(file).toMatchSnapshot();
 		}
 
