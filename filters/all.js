7e92493785ec881b48d4f6e4b3f0f2758e210c05
Krishna Kumar S <71367204+Krishks369@users.noreply.github.com>
2022-01-17 16:36:31 +0530

    chore: add eslint  (#158)

--- filters/all.js
+++ filters/all.js
@@ -10,12 +10,11 @@ function port(url, defaultPort) {
 filter.port = port;
 
 function pathResolve(pathName, basePath = '/') {
-  if(pathName.startsWith("/")){
-  return pathName;
-  }
-  else{
-    path.resolve(basePath,pathName);
-  }
+  if (pathName.startsWith('/')) {
+    return pathName;
+  }  
+  
+  path.resolve(basePath,pathName);
 }
 filter.pathResolve = pathResolve;
 
@@ -26,7 +25,7 @@ const parseOperationId = (channel, opName) => {
   const id = opName === 'subscribe' ? channel.subscribe().id() : channel.publish().id();
   if (!id) throw new Error('This template requires operationId to be set in every operation.');
   return id;
-}
+};
 
 function getOperationIds(channel) {
   const list = [];
